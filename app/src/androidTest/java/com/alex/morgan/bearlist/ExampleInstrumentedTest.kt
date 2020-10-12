03fed938f28c13db6650131f0f40f8e6c2a81990
Alex Morgan <axemorgan@gmail.com>
2020-10-12 17:26:58 -0400

    Change the package name to bearlist

--- app/src/androidTest/java/com/alex/morgan/bearlist/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/alex/morgan/bearlist/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.alex.morgan.kittenlist", appContext.packageName)
+        assertEquals("com.alex.morgan.bearlist", appContext.packageName)
     }
 }
